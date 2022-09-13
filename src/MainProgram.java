a7390ab7279fbdccd6d16df840e55be4cc4d4c18
rukimatt <rukimatt@kkg-do.eu>
2022-09-13 10:13:40 +0200

    commit

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -3,9 +3,8 @@ public class MainProgram {
     public static void main(String[] args) {
         Employee e1 = new Employee();
         Employee e2 = new Employee();
-        Employee e3 = new Employee();
-        Employee e4 = new Employee();
 
-
+        System.out.println(e1);
+        System.out.println(e2);
     }
 }
