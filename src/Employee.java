bd5e21e6ed789e9ecdaa9eec5333897826c6cf60
rukimatt <rukimatt@kkg-do.eu>
2022-09-13 10:25:39 +0200

    commit

--- src/Employee.java
+++ src/Employee.java
@@ -14,12 +14,51 @@ public class Employee {
     private int yearsOfService;
     private String specialties;
 
-    public Employee(){
+
+    public Employee(String name, String forename, String birthdate){
         this.yearsOfService = 0;
         this.salary = 1500;
         this.department = "A38";
         //Alle anderen Attribute/Referenzen erhalten ihren Nullwert.
+    }
 
+        public String getName() {
+            return name;
+    }
 
+    public String getForename() {
+        return forename;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getYearsOfService() {
+        return yearsOfService;
+    }
+
+    public String getSpecialties() {
+        return specialties;
     }
 }
--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,8 +1,8 @@
 public class MainProgram {
 
     public static void main(String[] args) {
-        Employee e1 = new Employee();
-        Employee e2 = new Employee();
+        Employee e1 = new Employee("Ambro", "1234", "01.01.1970");
+        Employee e2 = new Employee("Müller", "Dr.", "13.09.2022");
 
         System.out.println(e1);
         System.out.println(e2);
